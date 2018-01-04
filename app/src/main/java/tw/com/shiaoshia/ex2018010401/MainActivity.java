f09270506b4906fceef3380065403d81db5677d1
shiaoshia <34782716+shiaoshia@users.noreply.github.com>
2018-01-04 11:54:42 +0800

    建立LinearLayout

--- app/src/main/java/tw/com/shiaoshia/ex2018010401/MainActivity.java
+++ app/src/main/java/tw/com/shiaoshia/ex2018010401/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,13 +25,27 @@ public class MainActivity extends AppCompatActivity {
         //沒有用到layout 產生Button
         Button bnt = new Button(this);
         bnt.setText("OK");
-        setContentView(bnt);
         bnt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this,"Hello World!",Toast.LENGTH_SHORT).show();
             }
         });
+
+        Button bnt2 = new Button(this);
+        bnt2.setText("Canal");
+        bnt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"取消按鈕",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //建立LinearLayout
+        LinearLayout layout = new LinearLayout(this);
+        layout.addView(bnt);
+        layout.addView(bnt2);
+        setContentView(layout);
 
     }
 }
